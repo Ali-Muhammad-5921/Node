c3c2e9bba8614bb5bfa1bf95b18621ea88334169
Ali Muhammad <alimuhammad5921@gmail.com>
2026-01-16 15:30:09 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const number = 42;
+if (number > 10) {
+    console.log("The number is greater than 10.");
+} else {
+    console.log("The number is 10 or less.");
+}   
+console.log("This is a simple Node.js application.");
