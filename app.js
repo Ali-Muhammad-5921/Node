b40630ad92377ac72a1789c3ba8daff6080320e3
Ali Muhammad <alimuhammad5921@gmail.com>
2026-01-21 11:45:28 +0500

    express

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-// there are two ways to do it :-
 
-//      1.
-
-// const app = require('express')();
-
-//      2.
-
+const path = require('path');
 const express = require('express');
 const app = express();
 
-// Both ways are correct and will create an instance of an Express application.
-// The first way is a shorthand method that directly requires and invokes the Express module in one line.
-// The second way is more explicit, where you first require the Express module and then call it to create the app instance.
 
-// You can use either method based on your preference for code clarity or conciseness.
+// setup static and middleware
+app.use(express.static('./02-express-tutorial/public'));
 
-// we can use the following methods : 
-//      app.get()
-//      app.post()
-//      app.put()
-//      app.delete()
-//     app.all()
-//     app.use()
-//    app.listen()
+// app.get(('/'),(req,res) => {
+//     res.status(200).sendFile(path.resolve(__dirname,'./02-express-tutorial/navbar-app/index.html'));
+// }); we can remove this because of static middleware
+
+app.use((req,res) => {
+    res.status(404).send('Not Found');
+})
 
 
-
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-});
-app.get('/about', (req, res) => {
-    res.status(200).send('About Page');
-});
-app.use((req, res) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
-});
-
-
-
-app.listen(3000, () => {
+app.listen(3000,() => {
     console.log('Server is running on port 3000');
-});
+})
